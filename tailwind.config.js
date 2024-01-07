654b380157d0e2e77acd315f9fa906bec6fab366
Laura Corral López <laura.corral.next@bbva.com>
2024-01-07 17:50:41 +0100

    Added pages,styles and modifications

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content:  ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        colorWhite: '#ffffff',
+        colorPurple: '#3739b4',
+        colorLightPurple: '#ede8ff',
+        colorYellow: '#fc9b0a',
+        colorLightYellow: '#ffeed5',
+        colorGrey: '#ececec',
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        lexend: ["Lexend", "sans-serif"]
+      },
+      fontSize: {
+        xs: '0.5rem',
+        s: '0.75rem',
+        m: '1rem',
+        l: '1.5rem',
+        xl: '1.8rem',
+        xxl: '2.5rem',
+        xxxl: '4rem'
+      },
+      screens: {
+        'sm': '480px',
+        'md': '810px',
+        'lg': '1440px'
+      }
+    },
+  },
+  plugins: [],
+}
+
